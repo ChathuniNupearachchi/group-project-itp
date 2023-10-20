4db896586874f00b413ea243aa4f8dbdba2c752f
Sabaragamuwa S.A.T.D-IT21836268 <104703061+SabaragamuwaSATD@users.noreply.github.com>
2023-10-20 21:01:00 +0530

    Frontend_first_commit
    This is front-end part of wholesale customer management and Online store

---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+import { Form, Button} from 'react-bootstrap';
+import { saveShippingAddress } from '../slices/cartSlice';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+
+const ShippingScreen = () => {
+    const cart = useSelector((state) => state.cart);
+    const { shippingAddress } = cart;
+
+    const [address, setAddress] = useState(shippingAddress?.address || '');
+    const [city, setCity] = useState(shippingAddress?.city || '');
+    const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+    const [distric, setDistric] = useState(shippingAddress?.distric || '');
+
+   const navigate = useNavigate();
+   const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(saveShippingAddress({address, city, postalCode, distric}));
+        navigate('/payment');
+    };
+
+  return (
+    <FormContainer>
+        <CheckoutSteps step1 step2/>
+
+        <h1>Shipping</h1>
+
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId='address' className='my-2'>
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter address'
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                ></Form.Control> 
+            </Form.Group>
+
+            <Form.Group controlId='city' className='my-2'>
+            <Form.Label>City</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter city'
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                ></Form.Control> 
+            </Form.Group>
+
+            <Form.Group controlId='distric' className='my-2'>
+            <Form.Label>Distric</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter Distric'
+                    value={distric}
+                    onChange={(e) => setDistric(e.target.value)}
+                ></Form.Control> 
+            </Form.Group>
+
+            <Form.Group controlId='postalCode' className='my-2'>
+            <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                    type='Number'
+                    placeholder='Enter postal code'
+                    value={postalCode}
+                    onChange={(e) => setPostalCode(e.target.value)}
+                ></Form.Control> 
+            </Form.Group>
+
+            <Button type='submit' variant='primary' className='my-2'>
+                Continue
+            </Button>
+        </Form>
+    </FormContainer>
+  )
+}
+
+export default ShippingScreen
