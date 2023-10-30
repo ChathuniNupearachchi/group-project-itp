64bd62b3499d06427b8bcdeb16fac9fb23e49d5f
ChathuniNupearachchi <nachathunidilmini@gmail.com>
2023-10-30 12:19:36 +0530

    Delete folders

--- backend/routes/dilivaryStaffRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const registerStaff = require("../controllers/dilivaryStaffController");
-const router = express.Router();
-
-router.post("/", registerStaff);
-
-module.exports = router;
--- backend/routes/driverRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {fetchDrivers, getSingleDriver} = require("../controllers/driverController");
-const router = express.Router();
-
-router.get("/", fetchDrivers);
-router.get("/:id", getSingleDriver);
-
-module.exports = router;
